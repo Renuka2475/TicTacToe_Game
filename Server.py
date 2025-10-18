baeed3f70fa7445873cb662c4101992677b78b79
Renuka2475 <reddyrenuka2475@gmail.com>
2025-10-18 13:47:44 +0530

    Multiplayer Tic-Tac-Toe game.

--- Server.py
+++ Server.py
@@ -59,7 +59,7 @@ async def send_json(ws: WebSocket, data: dict):
     await ws.send_json(data)
 
 # ----- WebSocket endpoint -----
-@app.websocket("/")
+@app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     player_id = str(uuid.uuid4())
